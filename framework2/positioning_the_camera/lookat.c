0c08772690f4171a962f6b112eb8b587e9a7151a
GraciaM <MichelleGracia@gmail.com>
2016-02-10 13:28:15 +0100

    added comments

--- framework2/positioning_the_camera/lookat.c
+++ framework2/positioning_the_camera/lookat.c
@@ -25,6 +25,8 @@
 #define M_PI           3.14159265358979323846  /* pi */
 #endif
 
+
+// returns the norm of a vector
 double return_norm(GLdouble x, GLdouble y, GLdouble z)
 {
     return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
@@ -41,17 +43,22 @@ void myLookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ,
     
     // create camera coordinate system
     
+    // normalize & create cz
     double norm = return_norm(centerX, centerY, centerZ);
     GLdouble cz[] =  {centerX/norm, centerY/norm, centerZ/norm};
     
+    // calculate direction of cx
     GLdouble cx[] =  {cz[1]*upZ-cz[2]*upY, 
                       -cz[0]*upZ+cz[2]*upX, 
                       cz[0]*upY-cz[1]*upX}; 
+                      
+    // normalize cx                
     norm = return_norm(cx[0], cx[1], cx[2]);
     cx[0] = cx[0]/norm;
     cx[1] = cx[1]/norm;
     cx[2] = cx[2]/norm;
     
+    // calculate cy
     GLdouble cy[] =  {cx[1]*cz[2]-cx[2]*cz[1], 
                       -cx[0]*cz[2]+cx[2]*cz[0], 
                       cx[0]*cz[1]-cx[1]*cz[0]};

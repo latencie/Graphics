f276d1c2281703211b1f9a3ff880f5c8f21d3eba
GraciaM <MichelleGracia@gmail.com>
2016-02-10 16:17:17 +0100

    ortho

--- framework2/orthogonal_projection/ortho.c
+++ framework2/orthogonal_projection/ortho.c
@@ -26,11 +26,24 @@
 #define M_PI           3.14159265358979323846  /* pi */
 #endif
 
+
 void myOrtho(GLdouble left,
              GLdouble right,
              GLdouble bottom,
              GLdouble top,
              GLdouble near,
              GLdouble far) {
+             
+    // translate center to origin, and scale to screensize    
+    GLfloat O[16] = 
+    {
+        2/(right-left), 0., 0., 0.,
+        0, 2/(top-bottom), 0., 0.,
+        0., 0., -2/(far-near), 0.,
+        -(right+left)/(right-left), -(top+bottom)/(top-bottom), -(far+near)/(far-near), 1
+    };
+
+
+    glMultMatrixf(O);
 
 }
